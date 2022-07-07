91b4fe8059797fddfd5bda8e8889aa5295dca1e8
Ata FatahiBaarzi <afatahibaarzi@linkedin.com>
2022-07-06 23:25:48 -0400

    add first full impl of segments, sections, and symbols
    Signed-off-by: Ata FatahiBaarzi <afatahibaarzi@linkedin.com>

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
-	elf "github.com/mrata/goelf/pkg"
+	elfreader "github.com/mrata/goelf/pkg"
+	"log"
+	"os"
 )
 
 func main() {
 	fmt.Println("Start...")
-
-	elf.InitELF()
-
+	inputFile := "myelf"
+	fileContent, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatalf("Failed to read file: %s", err.Error())
+		os.Exit(1)
+	}
+	elf, err := elfreader.ParseFileContent(fileContent)
+	if err != nil {
+		log.Fatal(err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(elf.GetFileType())
+	elf.PrettyPrint()
 	fmt.Println("Done!")
 }
