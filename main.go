8c5771ad88c001477418dde5a0282b94be6d878a
Ata FatahiBaarzi <afatahibaarzi@linkedin.com>
2022-07-07 11:53:10 -0400

    add commandline option
    Signed-off-by: Ata FatahiBaarzi <afatahibaarzi@linkedin.com>
    Signed-off-by: Ata Fatahi <afatahibaarzi@linkedin.com>
    Signed-off-by: Ata Fatahi <immrata@gmail.com>

--- main.go
+++ main.go
@@ -1,26 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	elfreader "github.com/mrata/goelf/pkg"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Start...")
-	inputFile := "myelf"
+	var inputFile string
+	flag.StringVar(&inputFile, "file", "", "Input ELF File")
+
+	flag.Parse()
+	if inputFile == "" {
+		log.Fatalf("No input ELF file is provided.")
+	}
+
 	fileContent, err := os.ReadFile(inputFile)
 	if err != nil {
 		log.Fatalf("Failed to read file: %s", err.Error())
-		os.Exit(1)
 	}
 	elf, err := elfreader.ParseFileContent(fileContent)
 	if err != nil {
 		log.Fatal(err.Error())
-		os.Exit(1)
 	}
-	fmt.Println(elf.GetFileType())
 	elf.PrettyPrint()
-	fmt.Println("Done!")
+
 }
